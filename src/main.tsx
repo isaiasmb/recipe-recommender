e4724d1014759e655f705113914a2eadbb6939e8
Isaias Buscarino <isaiasbuscarino@hotmail.com>
2026-04-15 23:03:17 +0200

    feat: configure TanStack Query and update application branding
    - Integrated TanStack Query by wrapping the root component with QueryClientProvider.
    - Updated the document title and replaced default Vite branding with app-specific assets.
    - Added new image assets for ingredients, countries, and the application icon.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
+import { QueryClientProvider } from "@tanstack/react-query"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { queryClient } from "./query-client"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
 )
